ddcf2c9e728e3b4fd4d1af1d8a75ddd21d4e58bc
Kendra D. <kendra.dominique0@gmail.com>
2026-04-21 09:17:23 -0400

    refactor: create utils file and add get_user_choice function (#3)
    - Create utils file and import to other files.
    - Add get_user_choice function with input validation.

---
+++ utils.py
@@ -0,0 +1,10 @@
+def get_user_choice(dictionary):
+    dict_str = ", ".join(dictionary)
+    current_dataset = None
+    while current_dataset is None:
+        user_input = input(f"Type in which dataset you'd like to use ({dict_str}): ").lower()
+        if user_input not in dictionary:
+            print("Invalid input.")
+        else:
+            current_dataset = user_input
+    return current_dataset
