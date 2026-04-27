3564cf1408ad5fb73696aa7ebf11aad650433b3c
Kendra <kendra.dominique0@gmail.com>
2026-04-27 11:22:35 -0400

    test: add tests for plotting.py
    Add tests for plotting.py. Install pytest-mock plugin.

---
+++ tests/test_plotting.py
@@ -0,0 +1,17 @@
+from plotting import create_scatter_plot
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+def test_create_scatter_plot(mocker):
+    mock_plt = mocker.patch("plotting.plt")
+
+    X, y = datasets.make_circles(n_samples=1000, shuffle=True, noise=0.05, random_state=42, factor=0.8)
+    
+    current_dataset = "noisy_circles"
+
+    create_scatter_plot(X, y, current_dataset)
+
+    mock_plt.figure.assert_called_once()
+    mock_plt.scatter.assert_called_once()
+    mock_plt.title.assert_called_once_with(f"{current_dataset.capitalize()} dataset")
+    mock_plt.show.assert_called_once()
