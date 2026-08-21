5f33625e1784ba8b5f34d8b85b9771306928bdf2
Kendra D. <kendra.dominique0@gmail.com>
2026-08-21 11:09:16 -0400

    fix(ui): capitalize plot titles (#17)
    Previously, only the first word in the dataset plot titles was
    capitalized. Capitalize all words in the dataset plot titles.
    
    Update the test_plotting.py test suite accordingly.

--- plotting.py
+++ plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 def create_scatter_plot(X, y, current_dataset):
     plt.figure(figsize=(8, 6))
     plt.scatter(X[:, 0], X[:, 1], c=y, cmap='plasma', edgecolors='k')
-    plt.title(f"{current_dataset.capitalize()} dataset")
+    plt.title(f"{current_dataset.title()} Dataset")
     plt.show()
 
 def trigger_plot_creation(X, y, current_dataset):
