1026762b341a519e1d05cf0549e84ccb386ef612
Kendra <kendra.dominique0@gmail.com>
2026-08-25 16:49:28 -0400

    feat: add multi-step input form
    - Add home page as entry point
    - Add 4-step form flow to collect input for dataset, evaluation method,
      classifier, and normalization method; pass each field forward via hidden
      inputs
    - Use Jinja2 if/else to disable "artificial neural networks" classifier
      choice if evaluation method choice is leave-one-out, due to the
      computational cost of that combination

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def start():
+    return render_template("start_page.html")
+
+@app.route("/dataset")
+def select_dataset():
+    return render_template("dataset_selection.html")
+
+@app.route("/evaluation", methods=["POST"])
+def select_evaluation_method():
+    dataset = request.form["dataset"]
+    return render_template("evaluation_selection.html", dataset=dataset)
+
+@app.route("/classifier", methods=["POST"])
+def select_classifier():
+    dataset = request.form["dataset"]
+    evaluation = request.form["evaluation"]
+    return render_template("classifier_selection.html", dataset=dataset, evaluation=evaluation)
+
+@app.route("/normalization", methods=["POST"])
+def select_normalization_method():
+    dataset = request.form["dataset"]
+    evaluation = request.form["evaluation"]
+    classifier = request.form["classifier"]
+    return render_template("normalization_selection.html", dataset=dataset, evaluation=evaluation, classifier=classifier)
+
+@app.route("/results", methods=["POST"])
+def show_results():
+    return "Results to be added."
