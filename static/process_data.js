90f39dcf19917e5f7c68e74b9bfef6a4d92c44f4
Kendra <kendra.dominique0@gmail.com>
2026-09-02 11:59:43 -0400

    feat: auto-submit process page if only 1 result
    - Auto-submit the process page with the number of combinations set to 1
      if there is only one result, as the user wouldn't need to choose a
      number of combinations.
    - Display "Generating combination..." on that page in such case to
      reflect the auto-submit.

--- static/process_data.js
+++ static/process_data.js
@@ -2,6 +2,9 @@ let numericCombosBtn = document.getElementById("numeric-combos-btn");
 let numericCombosInput = document.getElementById("numeric-combos-input");
 let allCombosBtn = document.getElementById("all-combos-btn");
 let submitCombosBtn = document.getElementById("submit-combos-btn");
+let totalCombinations = document.getElementById("total-combinations");
+let form = document.querySelector("form");
+let loadingScreen = document.querySelector(".loading-screen");
 
 numericCombosBtn.addEventListener("click", () => {
     numericCombosInput.classList.remove("display-none");
@@ -13,3 +16,12 @@ numericCombosBtn.addEventListener("click", () => {
 allCombosBtn.addEventListener("click", () => {
     numericCombosInput.disabled = true;
 })
+
+if (totalCombinations.value == 1) {
+    loadingScreen.classList.remove("display-none");
+    console.log("combination is 1");
+    window.addEventListener("load", () => {
+        numericCombosInput.value = "1";
+        form.submit();
+    })
+}
